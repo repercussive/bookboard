e583a0e47f920c49236804cacdd830cd47467a08
= <liam2robertson@gmail.com>
2022-02-01 17:04:39 +0000

    🔍️ add page title to main app component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,13 +3,17 @@ import '@abraham/reflection'
 import '@/styles/globals.scss'
 import React from 'react'
 import initializeFirebase from '@/lib/firebase-setup/initializeFirebase'
+import Head from 'next/head'
 
 if (typeof window === 'undefined') React.useLayoutEffect = React.useEffect
 
 initializeFirebase()
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+function App({ Component, pageProps }: AppProps) {
+  return <>
+    <Head><title>Bookboard</title></Head>
+    <Component {...pageProps} />
+  </>
 }
 
-export default MyApp
+export default App
